8b5f7082cc1ca4088e73515db87d330666511939
mrkarimoff <mrkarimoff99@gmail.com>
2023-10-03 21:22:43 +0500

    still trying to fix deployment error

--- app/[project]/_components/Navigations.tsx
+++ app/[project]/_components/Navigations.tsx
@@ -1,6 +1,8 @@
 import { fetchFileSystemData } from "@/lib/docs";
 import NavigationSwitcher from "./NavigationSwitcher";
 
+export const runtime = "nodejs";
+
 export default function Navigations(): JSX.Element {
   const fileSystemData = fetchFileSystemData(`docs`);
 
--- app/_components/Sidebar.tsx
+++ app/_components/Sidebar.tsx
@@ -1,5 +1,7 @@
 import Navigations from "@/app/[project]/_components/Navigations";
 
+export const runtime = "nodejs";
+
 const Sidebar = () => {
   return (
     <div className="sticky top-1">
