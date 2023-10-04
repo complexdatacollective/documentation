86a4be7f1f4428c04de1f6a29440d8fe00be6f0d
Joshua Melville <joshua.melville@northwestern.edu>
2023-10-04 12:18:22 +0200

    update mdxfile.tsx to use NEXT_PUBLIC_DOCS_PATH

--- lib/MDXFile.tsx
+++ lib/MDXFile.tsx
@@ -1,6 +1,8 @@
 import dynamic from "next/dynamic";
 
 export default function MDXFile({ name }: { name: string }) {
-  const ServerComponent = dynamic(() => import(`@/public/docs/${name}.mdx`));
+  const ServerComponent = dynamic(
+    () => import(`${process.env.NEXT_PUBLIC_DOCS_PATH}/${name}.mdx`)
+  );
   return <ServerComponent />;
 }
