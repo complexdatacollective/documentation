d0647dc6551d59d4068888a32cdcbf95b17bda6e
mrkarimoff <mrkarimoff99@gmail.com>
2023-10-18 18:41:11 +0500

    trying to find the deployment problem source with search command menu by updating the code

--- app/[project]/_components/Search/SearchCommand.tsx
+++ app/[project]/_components/Search/SearchCommand.tsx
@@ -5,10 +5,7 @@ import { Button } from "@/components/ui/button";
 import {
   Command,
   CommandDialog,
-  CommandEmpty,
   CommandGroup,
-  CommandInput,
-  CommandItem,
   CommandList,
   CommandSeparator,
 } from "@/components/ui/command";
@@ -48,41 +45,11 @@ export default function SearchCommand() {
       </Button>
 
       <CommandDialog open={open} onOpenChange={setOpen}>
-        <CommandInput placeholder="Type a command or search..." />
-        <CommandList>
-          <CommandEmpty>No results found.</CommandEmpty>
-          <CommandGroup heading="Suggestions">
-            <CommandItem>
-              <span>Calendar</span>
-            </CommandItem>
-            <CommandItem>
-              <span>Search Emoji</span>
-            </CommandItem>
-            <CommandItem>
-              <span>Calculator</span>
-            </CommandItem>
-          </CommandGroup>
-          <CommandSeparator />
-          <CommandGroup heading="Settings">
-            <CommandItem>
-              <span>Profile</span>
-            </CommandItem>
-            <CommandItem>
-              <span>Billing</span>
-            </CommandItem>
-            <CommandItem>
-              <span>Settings</span>
-            </CommandItem>
-          </CommandGroup>
-        </CommandList>
-      </CommandDialog>
-
-      {/* <InstantSearchNext
-        searchClient={algolia_client}
-        indexName={process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME!}
-        insights={true}
-      >
-        <CommandDialog open={open} onOpenChange={setOpen}>
+        <InstantSearchNext
+          searchClient={algolia_client}
+          indexName={process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME!}
+          insights={true}
+        >
           <CustomSearchBox />
           <CommandSeparator />
 
@@ -94,8 +61,8 @@ export default function SearchCommand() {
               </EmptyQueryBoundary>
             </CommandGroup>
           </CommandList>
-        </CommandDialog>
-      </InstantSearchNext> */}
+        </InstantSearchNext>
+      </CommandDialog>
     </>
   );
 }
