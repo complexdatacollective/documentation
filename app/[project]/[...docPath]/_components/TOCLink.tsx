4c95117fb75b38e7388241c1ab77c71caa5647e2
mrkarimoff <mrkarimoff99@gmail.com>
2023-10-24 19:05:08 +0500

    added auto scroll to the TOC component

--- app/[project]/[...docPath]/_components/TOCLink.tsx
+++ app/[project]/[...docPath]/_components/TOCLink.tsx
@@ -1,12 +1,21 @@
 import useHighlighted from "@/hooks/useHighlighted";
 import { type HeadingNode } from "@/lib/tableOfContents";
 import Link from "next/link";
+import { useEffect, useRef } from "react";
 
 const TOCLink = ({ node }: { node: HeadingNode }) => {
+  const ref = useRef<HTMLAnchorElement>(null);
   const [highlighted] = useHighlighted(node.data.id);
+
+  useEffect(() => {
+    if (highlighted && ref.current) {
+      ref.current.scrollIntoView({ behavior: "auto", block: "nearest", inline: "nearest" });
+    }
+  }, [highlighted]);
 
   return (
     <Link
+      ref={ref}
       href={`#${node.data.id}`}
       className={`block ${
         node.depth === 2 ? "text-sm lg:text-base" : "text-xs lg:text-sm"
