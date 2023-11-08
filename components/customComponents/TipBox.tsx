281e0389efbf5ca03c1fa360ca724db7b3bf5150
mrkarimoff <mrkarimoff99@gmail.com>
2023-11-08 13:02:21 +0500

    added danger/info state to TipBox

--- components/customComponents/TipBox.tsx
+++ components/customComponents/TipBox.tsx
@@ -1,14 +1,21 @@
 import React, { type ReactNode } from 'react';
 import { Terminal, Lightbulb } from 'lucide-react';
-
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 
-const TipBox = ({ children }: { children: ReactNode }) => {
+type TipBoxProps = {
+  children: ReactNode;
+  danger: boolean;
+};
+
+const TipBox = ({ children, danger }: TipBoxProps) => {
   return (
-    <Alert className="my-5 border border-red-400">
+    <Alert
+      variant={danger ? 'destructive' : 'default'}
+      className={`my-5 border ${danger ? 'border-red-400' : 'border-blue-500'}`}
+    >
       <Terminal className="h-4 w-4" />
       <AlertTitle>
-        <Lightbulb />
+        <Lightbulb className={`${danger ? 'text-red-400' : 'text-blue-500'}`} />
       </AlertTitle>
       <AlertDescription>{children}</AlertDescription>
     </Alert>
