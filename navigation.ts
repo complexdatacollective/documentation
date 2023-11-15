c0c69da436c4feb0a2d57d366dd287cb0c547077
mrkarimoff <mrkarimoff99@gmail.com>
2023-11-15 15:43:03 +0500

    changed the source of truth for supported locales

--- navigation.ts
+++ navigation.ts
@@ -1,13 +1,5 @@
 import { createSharedPathnamesNavigation } from 'next-intl/navigation';
-import { env } from './env.mjs';
-
-type Locales = string[];
-
-export const locales: Locales = JSON.parse(
-  env.NEXT_PUBLIC_LANGUAGES,
-) as Locales;
-
-// export const locales = ['en', 'ru'] as const;
+import { locales } from './locales.mjs';
 
 export const { Link, redirect, usePathname, useRouter } =
   createSharedPathnamesNavigation({ locales });
