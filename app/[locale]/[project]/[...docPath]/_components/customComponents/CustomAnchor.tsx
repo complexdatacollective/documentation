0002a5dd96a5720132bf0886c1e0f2ea6719c9bf
mrkarimoff <mrkarimoff99@gmail.com>
2023-11-17 22:59:32 +0500

    temporarily assigned CustomAnchor props type to any

--- app/[locale]/[project]/[...docPath]/_components/customComponents/CustomAnchor.tsx
+++ app/[locale]/[project]/[...docPath]/_components/customComponents/CustomAnchor.tsx
@@ -1,17 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-console */
 import Link from 'next/link';
-import { type DetailedHTMLProps, type HTMLAttributes } from 'react';
 
-interface CustomAnchorProps
-  extends DetailedHTMLProps<
-    HTMLAttributes<HTMLAnchorElement>,
-    HTMLAnchorElement
-  > {
-  href: string;
-}
-
-const CustomAnchor = (props: CustomAnchorProps) => {
+const CustomAnchor = (props: any) => {
   return <Link href={props.href}>{props.children}</Link>;
 };
 
 export default CustomAnchor;
+
+// Todo: fix type of CustomAnchor later
+// The below type is not working
+// DetailedHTMLProps<HTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>;
